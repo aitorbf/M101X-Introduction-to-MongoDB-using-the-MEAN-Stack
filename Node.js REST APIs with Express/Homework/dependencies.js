a1b0fc87530922d813280ba38373f400e75cfc60
aitorbf <aitorbf@gmail.com>
2016-03-15 18:53:07 +0100

    Module 3 with homework

---
+++ Node.js REST APIs with Express/Homework/dependencies.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+var fx = require('./fx');
+var Stripe = require('stripe');
+
+module.exports = function(wagner) {
+  wagner.factory('Config', function() {
+    return JSON.parse(fs.readFileSync('./config.json').toString());
+  });
+
+  var stripe = wagner.invoke(function(Config) {
+    return Config.stripeKey;
+  });
+
+  // TODO: Make Stripe depend on the Config service and use its `stripeKey`
+  // property to get the Stripe API key.
+  wagner.factory('Stripe', function() {
+    return Stripe(stripe);
+  });
+
+  wagner.factory('fx', fx);
+};
